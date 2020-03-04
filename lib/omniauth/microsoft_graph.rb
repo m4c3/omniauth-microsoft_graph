d1a75fea577771be3a5e6e622623e167da94f5dc
Matthias Haehnel <matthias.haehnel@sixt.com>
2020-03-04 07:28:40 +0100

    rubocoped

--- lib/omniauth/microsoft_graph.rb
+++ lib/omniauth/microsoft_graph.rb
@@ -1,2 +1,4 @@
-require "omniauth/microsoft_graph/version"
-require "omniauth/strategies/microsoft_graph"
+# frozen_string_literal: true
+
+require 'omniauth/microsoft_graph/version'
+require 'omniauth/strategies/microsoft_graph'
--- lib/omniauth/microsoft_graph/version.rb
+++ lib/omniauth/microsoft_graph/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Omniauth
   module MicrosoftGraph
-    VERSION = "0.2.1"
+    VERSION = '0.2.1'
   end
 end
