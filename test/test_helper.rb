1485eb867a8bd009efa417b3c8d164c13a66a0dd
Matthias Haehnel <matthias.haehnel@sixt.com>
2020-03-04 07:25:05 +0100

    adatped tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'bundler/setup'
 require 'minitest/autorun'
 require 'mocha/setup'
@@ -14,18 +16,18 @@ class StrategyTestCase < Minitest::Test
     @request.stubs(:scheme).returns({})
     @request.stubs(:ssl?).returns(false)
 
-    @client_id = '123'
-    @client_secret = '53cr3tz'
+    @client_id = '00000000-1111-1111-1111-000000000000'
+    @client_secret = 'asdfasdfasdfasdfasdfasdfasdfasdf'
+    @tenant_id = '00000000-0000-0000-0000-000000000000'
     @options = {}
   end
 
   def strategy
     @strategy ||= begin
-      args = [@client_id, @client_secret, @options].compact
+      args = [@client_id, @client_secret, @tenant_id, @options].compact
       OmniAuth::Strategies::MicrosoftGraph.new(nil, *args).tap do |strategy|
         strategy.stubs(:request).returns(@request)
       end
     end
   end
 end
-
