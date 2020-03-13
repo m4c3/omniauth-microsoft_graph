463a5fc976a39364591f52ed13bf7fc7fda7a690
Matthias Haehnel <matthias.haehnel@sixt.com>
2020-03-13 08:42:18 +0100

    next try to simplify

--- lib/omniauth/strategies/microsoft_graph.rb
+++ lib/omniauth/strategies/microsoft_graph.rb
@@ -7,42 +7,24 @@ module OmniAuth
     class MicrosoftGraph < OmniAuth::Strategies::OAuth2
       BASE_AZURE_URL = 'https://login.microsoftonline.com'
 
-      option :scopes, ['openid', 'profile', 'offline_access']
       option :name, :microsoft_graph
-      option :tenant_provider, nil
-      args [:tenant_provider]
+      option :client_id
+      option :client_secret
+      option :tenant_id, 'common'
+      option :scopes, 'openid profile offline_access'
       option :extensions, nil
+      option :authorize_options, %i[display score auth_type scope prompt login_hint domain_hint response_mode]
+
+
 
       def client
-        provider = if options.tenant_provider
-                     options.tenant_provider.new(self)
-                   else
-                     options # if pass has to config, get mapped right on to options
-                   end
+        options.scope = option.scopes.join(' ')
+        options.base_azure_url = BASE_AZURE_URL
 
-        options.scope = provider.scopes.join(' ')
-        options.client_id = provider.client_id
-        options.client_secret = provider.client_secret
-        options.tenant_id =
-          provider.respond_to?(:tenant_id) ? provider.tenant_id : 'common'
-        options.base_azure_url =
-          provider.respond_to?(:base_azure_url) ? provider.base_azure_url : BASE_AZURE_URL
-
-        if provider.respond_to?(:authorize_params)
-          options.authorize_params = provider.authorize_params
-        end
-        if provider.respond_to?(:domain_hint) && provider.domain_hint
-          options.authorize_params.domain_hint = provider.domain_hint
-        end
-        if defined? request && request.params['prompt']
-          options.authorize_params.prompt = request.params['prompt']
-        end
         options.client_options.authorize_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/authorize"
         options.client_options.token_url = "#{options.base_azure_url}/#{options.tenant_id}/oauth2/v2.0/token"
         super
       end
-
-      option :authorize_options, %i[display score auth_type scope prompt login_hint domain_hint response_mode]
 
       uid { raw_info['id'] }
 
@@ -73,10 +55,10 @@ module OmniAuth
         @memberships ||= access_token.get('https://graph.microsoft.com/v1.0/me/memberOf?$select=displayName').parsed
       end
       def extensions
-        #if options.extensions 
+        #if options.extensions
          Rails.logger.warn "extensions:" + options.extensions
          @extensions ||= access_token.get('https://graph.microsoft.com/v1.0/me?$select=' + options.extensions).parsed
-        Rails.logger.warn "extensions:" + @extension
+         Rails.logger.warn "extensions:" + @extension
         #end
       end
 
