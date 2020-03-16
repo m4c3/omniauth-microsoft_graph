350257b1d0719aba57a425eaa56472645ebfdfc3
Matthias Haehnel <matthias.haehnel@sixt.com>
2020-03-16 08:39:43 +0100

    remove debug log

--- lib/omniauth/strategies/microsoft_graph.rb
+++ lib/omniauth/strategies/microsoft_graph.rb
@@ -76,8 +76,6 @@ module OmniAuth
       def extensions
         if options[:extensions]
          @extensions ||= access_token.get('https://graph.microsoft.com/v1.0/me?$select=' + options[:extensions]).parsed
-         Rails.logger.warn "extensions:" + @extensions.to_s
-         @extensions
         end
       end
 
